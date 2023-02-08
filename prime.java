50130efa83f07cf64138c964bdf7758b2a8d91f3
gautham963 <124800882+gautham963@users.noreply.github.com>
2023-02-08 07:07:28 -0800

    Create prime.java

---
+++ prime.java
@@ -0,0 +1,17 @@
+package as1;
+
+import java.util.Scanner;
+
+public class prime {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int  number;
+		System.out.println("Enter the number:");
+		Scanner s=new Scanner(System.in);
+		number=s.nextInt();
+		
+
+	}
+
+}
